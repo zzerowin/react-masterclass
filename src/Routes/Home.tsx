ef4657362e107f545b317529ed28d574554d17cc
zzerowin <alaykr123@gmail.com>
2022-03-03 17:43:05 +0900

    Box Animations

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -57,7 +57,28 @@ const Box = styled(motion.div)<{ bgPhoto: string;}>`
   background-position: center center;
   height: 200px;
   font-size: 66px;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
 `;
+
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${props => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+    
+  }
+`;
+
 
 const rowVariants = {
   hidden: {
@@ -69,6 +90,32 @@ const rowVariants = {
   exit: {
     x: -window.outerWidth - 10,
   },
+}
+
+const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: {
+      delay: 0.5,
+      duration: 0.3,
+      type: "tween",
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.3,
+      type: "tween",
+    },
+  }
 }
 
 const offset = 6;
@@ -118,8 +165,16 @@ function Home() {
                 .map((movie) => (
                   <Box
                     key={movie.id}
+                    whileHover="hover"
+                    initial="normal"
+                    variants={boxVariants}
+                    transition={{type: "tween"}}
                     bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
-                  />
+                  >
+                    <Info variants={infoVariants}>
+                      <h4>{movie.title}</h4>
+                    </Info>
+                  </Box>
                 ))}
             </Row>
           </AnimatePresence>
